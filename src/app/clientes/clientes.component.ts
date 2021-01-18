773f39546b77c8ad9616e353d6f25ae901289f8e
Gabriel <campi.utn.gabriel@gmail.com>
2021-01-18 18:13:38 -0300

    2

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente} from '../shared/cliente';
+import {ClienteService} from '../shared/cliente.service';
+import{MatTable} from '@angular/material/table';
 
 @Component({
   selector: 'app-clientes',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
+  clientes: Cliente[] = [];
+  editando = false;
 
-  constructor() { }
+  constructor(private clienteService: ClienteService ) { }
+
 
   ngOnInit(): void {
+    this.getClientes()
+  }
+  delete( row : Cliente){
+  }
+  getClientes():void{
+    this.clienteService.get().subscribe(
+      (clientes) => {
+        this.clientes = clientes
+      }
+    )
+  }
+
+  edit( row : Cliente){
+    this.editando = true;
+  }
+
+  aceptar(){
+    this.editando = false;
+  }
+  cancelar(){
+    this.editando = false;
   }
 
 }
---
+++ src/app/shared/cliente.ts
@@ -0,0 +1,8 @@
+export class Cliente {
+  cliId: number = 0;
+  cliNombre: string = '';
+  cliApellido: string = '';
+  cliDireccion: string = '';
+  cliFechaAlta: Date = new Date();
+  cliBorrado: boolean = false;
+}
